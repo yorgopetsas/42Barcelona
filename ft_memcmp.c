f8f5ab8259a917c4ec08853757253419e84cb97c
Yorgo Zisis petsas <yzisis-p@cbr9s2.42barcelona.com>
2022-09-09 18:38:23 +0200

    memcmp final

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,51 +17,15 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	const unsigned char	*sh;
 	size_t				i;
 
-	sh = s1;
-	ch = s2;
+	sh = (const unsigned char *)s1;
+	ch = (const unsigned char *)s2;
 	i = 0;
-// 	if (n == 0 || (*sh == 0 && *ch == 0))
-// 		return (0);
-	if (!*sh)
-		return (-1);
-	if (!*ch)
-		return (1);
-	while (sh[i] == ch[i] && i < n && sh[i] != '\0' && ch[i] != '\0')
+	
+	while (i < n)
+	{
+		if (sh[i] != ch[i])
+			return (sh[i] - ch[i]);
 		i++;
-	if (sh[i] == '\0' && ch[i] == '\0')
-		return (sh[i - 1] - ch[i - 1]);
-	if (sh[i - 1] == ch[i - 1] && i == n)
-		return (0);
-	if (sh[i] < ch[i])
-		return (-1);
-	if (sh[i] > ch[i])
-		return (1);
-	return (sh[i] - ch[i]);
+	}
+	return (0);
 }
-
-
-// NAME
-//      memcmp -- compare byte string
-// 
-// LIBRARY
-//      Standard C Library (libc, -lc)
-// 
-// SYNOPSIS
-//      #include <string.h>
-// 
-//      int
-//      memcmp(const void *s1, const void *s2, size_t n);
-// 
-// DESCRIPTION
-//      The memcmp() function compares byte string s1 against byte string s2.  Both strings are assumed to be n bytes long.
-// 
-// RETURN VALUES
-//      The memcmp() function returns zero if the two strings are identical, otherwise returns the difference between the first two differing bytes (treated as
-//      unsigned char values, so that `\200' is greater than `\0', for example).  Zero-length strings are always identical.  This behavior is not required by C and
-//      portable code should only depend on the sign of the returned value.
-// 
-// SEE ALSO
-//      bcmp(3), strcasecmp(3), strcmp(3), strcoll(3), strxfrm(3), wmemcmp(3)
-// 
-// STANDARDS
-//      The memcmp() function conforms to ISO/IEC 9899:1990 (``ISO C90'').
