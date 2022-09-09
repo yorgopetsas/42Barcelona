e61f4ed55d0a4255404dbf440e4a45283184e07c
Yorgo Zisis petsas <yzisis-p@cbr9s2.42barcelona.com>
2022-09-09 18:10:12 +0200

    new strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,54 +11,24 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-char *	yz_filters(const char *haystack, const char *needle, size_t len)
-{
-	if (needle == NULL)
-		return ((char *)haystack);
-	if (len < 0)
-		return ((char *)needle);
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	else
-		return ("c");
-}
-
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	l;
 
 	i = 0;
-	j = 0;
-	l = 0;
-	if (yz_filters != "c")
-		break;
-	while (*haystack != '\0' && len > 0)
+	if (*needle == '\0' || needle == NULL)
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
 	{
-		if (needle[i] == *haystack)
+		j = 0;
+		while (needle[j] == haystack[i + j] && i + j < len)
 		{
-			l = len;
-			while (needle[i] == haystack[j]
-				&& needle[i] != '\0' && haystack[j] && l > 0)
-			{
-				i++;
-				j++;
-				l--;
-			}
-			if (needle[i - 1] == haystack[j - 1] && needle[i] == '\0')
-				return ((char *)haystack);
-			i = 0;
-			j = 0;
-			haystack++;
-			len--;
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
 		}
-		else
-		{
-			haystack++;
-			len--;
-		}
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
-
