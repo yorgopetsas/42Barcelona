f4328b23b396fd325060bb4c34642827f04368e2
Yorgo Petsas <yorgopecas@gmail.com>
2022-09-21 16:16:38 +0200

    get_net_line first steps

---
+++ GetNextLine/get_next_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_netx_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yzisis-p <yzisis-p@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 23:14:31 by yzisis-p          #+#    #+#             */
+/*   Updated: 2022/08/01 23:14:34 by yzisis-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_net_line.h"
+
+char	*get_next_line(int fd)
+{
+
+}
+
+//We are going to create a function that will take an input content from a file 
+//and return one line at a time 
+//PREGUNTAS QUE DEBES PODER CONTESTAR EN TU EVLUACION
+//¿Funciona correctamente tu get_next_line si el BUFFER_SIZE es 9999?
+//¿Y si es 1? ¿Qué tal con 10000000? ¿Sabes por qué?
+//¿Sabes por qué?
