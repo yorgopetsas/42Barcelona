56e590a1c435a92b75966eecaf1504a9f133fc77
Yorgo Petsas <yorgopecas@gmail.com>
2022-11-03 18:43:20 +0100

    improve order and comments of header file

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,33 +6,38 @@
 /*   By: yzisis-p <yzisis-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 20:14:58 by yzisis-p          #+#    #+#             */
-/*   Updated: 2022/10/20 15:02:32 by yzisis-p         ###   ########.fr       */
+/*   Updated: 2022/11/03 18:42:04 by yzisis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+// LIBRARY FOR READ()
+# include <unistd.h>
+// LIBRARY FOR MALLOC()
+# include <stdlib.h>
+// OTHER LIBRARIES
+# include <limits.h>
 # include <fcntl.h>
 # include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 2
 # endif
 
+/*~~~~~~~~~~~~~~~~GET_NEXT_LINE~~~~~~~~~~~~~~~~*/
 char	*get_next_line(int fd);
 char	*yz_load_buffer(int fd, char *buffer);
 char	*yz_join_n_free(char *buffer, char *tmp);
-char	*ft_strjoin(char *s1, char *s2);
-int		ft_strlcpy(char *dest, char *src, size_t size);
-int		ft_strchr(const char *str, int ch);
-// int		ft_strchr(const char *s, int c);
-int		ft_strlen(const char *str);
 char	*yz_buffer_to_line(char *buffer);
 char 	*yz_buffer_set(char *buffer);
+
+/*~~~~~~~~~~~~~~~~GET_NEXT_LINE_UTILS~~~~~~~~~~~*/
+int		ft_strlen(const char *str);
+int		ft_strchr(const char *str, int ch);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlcpy(char *dest, char *src, size_t size);
 char	*ft_substr(char *s, int start, int len);
 
 #endif
