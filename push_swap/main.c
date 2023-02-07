08564ab4df8750854290d855c8f627d6f5aedde8
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-08 00:01:17 +0100

    Stacks A & B created, memory allocated, protected and freed, library started. All tested

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,28 +6,106 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 12:04:13 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/07 12:08:14 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/07 23:59:39 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "push_swap.h"
 
-#include <stdio.h>
 
-int ft_push_swap(int a ,int b)
+t_stack *stack_mem(t_stack *stack)
 {
-
-
+	stack = malloc(sizeof(t_stack) * 1);
+	if (!stack)
+		return (stack);
+	return (stack);
 }
 
-void	main()
+t_stack	fill_stack(t_stack *stack, int idx, char **argv)
 {
-	int *a;
-	int *b;
-
-	a = ['1', '7', '3', '6', '12', '5']
-	b = []
-	ft_push_swap(a, b);
-	printf('The resutl is %d', int b);
+	stack->num = (argv[idx][0] - 48);
+	stack->index = idx;
+	stack->next = NULL;
+	return(stack[0]);
 }
 
+ssize_t	startup(char **argv, int idx)
+{
+	t_stack *stack_a;
+	t_stack *stack_b;
+
+	stack_a = NULL;
+	stack_b = NULL;
+
+	// RESERVE MEMORY FOR THE STACKS
+	stack_a = stack_mem(stack_a);
+	stack_b = stack_mem(stack_b);
+
+	// FILL STACKS
+	*stack_a = fill_stack(stack_a, idx, argv);
+	*stack_b = fill_stack(stack_a, idx, argv);	
+
+	// PRINT STACKS
+
+	printf("Stack A Position: %zu Value is %d\t|\tStack B Position: %zu Value is %d\n", 
+		stack_a->index, stack_a->num, stack_b->index, stack_b->num);
+
+	printf("Next element is %p\n", (stack_a->next));
+	// FREE STACKS
+	// free(&stack_a);
+	// free(&stack_b);
+	return (1);
+}
+
+// size_t	check_input(char **input);
 
 
+// FUNTION TO SHOW THE ERROR MSG AND RETURN -1
+size_t	ft_error_free(t_stack **stack_a, t_stack **stack_b)
+{
+	// ft_free(stack_a);
+	// ft_free(stack_b);
+	return (0);
+}
+
+// FUNTION TO SHOW THE ERROR MSG AND RETURN -1
+size_t	ft_error(void)
+{
+	printf("3\n");
+	write(2, "Error\n", 6);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	printf("Number of input is %d\n", argc - 1);
+	int idx;
+	idx = 1;
+	while (argv[idx] != '\0')
+	{
+		startup(argv, idx);
+		idx++;
+		// printf("ARGV is %c\n", argv[idx][0]);
+	}
+
+	// BRUNOS WAY STEP 1
+	// if (argc > 1)
+	// {
+	// 	if (!startup(argv))
+	// 	{
+	// 		printf("1\n");
+	// 		return (ft_error());
+	// 	}
+	// 	else
+	// 	{
+	// 		printf("2\n");
+	// 		return (ft_error());
+	// 	}
+	// }
+	// else
+	// {
+	// 	return(0);
+	// }
+
+	exit (0);
+	return (0);
+}
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:36:39 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/07 15:50:07 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/07 23:36:55 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 # define PUSH_SWAP_H
 
 # include <stdio.h>
+// TMP
+#include <unistd.h>
+#include <stdlib.h>
 
 typedef struct s_stack
 {
 	int	num;
 	size_t	index;
-	struck s_stack	*next;
+	struct s_stack	*next;
 }	t_stack;
 
 #endif
+
+ssize_t	startup(char **argv, int idx);
+
+t_stack	fill_stack(t_stack *stack_a, int idx, char **argv);
+t_stack *stack_mem(t_stack *stack);
+
+//
+size_t	ft_error_free(t_stack **stack_a, t_stack **stack_b);
+size_t	ft_error(void);
