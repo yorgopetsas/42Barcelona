33742a808dd5b5f3cdbdb3e020eb3febb540fd31
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-19 17:05:29 +0100

    Case Three Working and Style Fixed

--- push_swap/case_three_scenarios.c
+++ push_swap/case_three_scenarios.c
@@ -6,38 +6,38 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:33:06 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/19 16:46:31 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/19 17:04:36 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
-void scenario_1(t_stack **stack)
+void	scenario_1(t_stack **stack)
 {
 	ft_sw(stack);
 	exit(0);
 }
 
-void scenario_2(t_stack **stack)
+void	scenario_2(t_stack **stack)
 {
 	ft_sw(stack);
 	ft_rrab(stack, stack);
 	exit(0);
 }
 
-void scenario_3(t_stack **stack)
+void	scenario_3(t_stack **stack)
 {
 	ft_rab(stack);
 	exit(0);
 }
 
-void scenario_4(t_stack **stack)
+void	scenario_4(t_stack **stack)
 {
 	ft_sw(stack);
 	ft_rab(stack);
 	exit(0);
 }
 
-void scenario_5(t_stack **stack)
+void	scenario_5(t_stack **stack)
 {
 	ft_rrab(stack, stack);
 	exit(0);
