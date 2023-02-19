a9c604e010883d5ae2b3f7a26ed173e179d44b97
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-19 17:03:13 +0100

    Fixes of Styling Requierments

--- push_swap/case_three.c
+++ push_swap/case_three.c
@@ -6,40 +6,34 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 14:24:13 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/19 16:39:25 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/19 17:02:08 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 void	case_three(t_stack **stack)
 {
-	int a;
-	int b;
-	int c;
+	int	a;
+	int	b;
+	int	c;
 
 	a = (*stack)->num;
 	b = (*stack)->next->num;
 	c = (*stack)->next->next->num;
-
 	if (a > b)
 	{
 		if (b < c && c > a)
 			scenario_1(stack);
-
 		else if (b > c && c < a)
 			scenario_2(stack);
-		
 		else if (b < c && c < a)
 			scenario_3(stack);
 	}
-
 	if (a < b)
 	{
 		if (b > c && c > a)
 			scenario_4(stack);
-
 		else if (b > c && c < a)
 			scenario_5(stack);
-
 	}
 }
