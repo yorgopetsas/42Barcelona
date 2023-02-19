1776dd571392bcb33348e5a89bc63e5f50a0fa79
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-19 14:27:33 +0100

    Case Controler Organized

--- push_swap/case-controler.c
+++ push_swap/case-controler.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cases.c                                            :+:      :+:    :+:   */
+/*   case-controler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 22:59:44 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/19 14:20:24 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/19 14:27:03 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -23,65 +23,4 @@ void	ft_controler(t_stack **stack, int argc)
 		 case_hun(stack);
 	if (argc <= 502)
 		 case_fhun(stack);
-}
-
-void	case_two(t_stack **stack)
-{
-	// printf("2 arguments case\n");
-	// printf("stack->num: %d, \n", (*stack)->num);
-	// printf("stack->next->num: %d, \n", (*stack)->next->num);
-	if ((*stack)->num > (*stack)->next->num)
-	{
-		ft_rab(stack);
-		printf("ra\n");
-	}
-	// show_stack(stack, stack);
-}
-
-// I compare top to middle, middle to bottom, and bottom to top
-void	case_three(t_stack **stack)
-{
-	
-	if (!((*stack)->num > (*stack)->next->num))
-	 	if (!((*stack)->next->num > (*stack)->next->next->num))
-	 	{
-	 		if (!((*stack)->next->next->num < (*stack)->num))
-	 		{
-	 			printf("All Good!\n");
-	 			show_stack(stack, stack);
-	 			exit(0);
-	 		}
-	 	}
-	 	printf("No Good2!\n");
-	 	ft_rrab(stack, stack);
-	 	printf("RRAB\n");
-	 	if (((*stack)->num > (*stack)->next->num))
-	 	{
-		 	// case_three(stack);
-			ft_sw(stack);
-			printf("SW\n");
-			show_stack(stack, stack);
-		}
-	ft_rrab(stack, stack);
-	printf("RRAB\n");
-	show_stack(stack, stack);
-	if (!((*stack)->num > (*stack)->next->num))
-		printf("No Good3!\n");
-	printf("Good3!\n");
-	// show_stack(stack, stack);
-}
-
-void	case_five(t_stack **stack)
-{
-	printf("5 arguments case\n");
-}
-
-void	case_hun(t_stack **stack)
-{
-	printf("100 arguments case\n");
-}
-
-void	case_fhun(t_stack **stack)
-{
-	printf("500 arguments case\n");
 }
