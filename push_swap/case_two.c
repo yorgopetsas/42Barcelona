49033c5b793ea23cbb938844f585970b19b6c8be
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-19 14:55:03 +0100

    Case Two Clean Up

--- push_swap/case_two.c
+++ push_swap/case_two.c
@@ -6,20 +6,16 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 14:25:12 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/19 14:25:32 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/19 14:54:41 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 void	case_two(t_stack **stack)
 {
-	// printf("2 arguments case\n");
-	// printf("stack->num: %d, \n", (*stack)->num);
-	// printf("stack->next->num: %d, \n", (*stack)->next->num);
 	if ((*stack)->num > (*stack)->next->num)
 	{
 		ft_rab(stack);
 		printf("ra\n");
 	}
-	// show_stack(stack, stack);
 }
