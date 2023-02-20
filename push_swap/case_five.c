b840dbc6cd7e072eb01e1a8a0866d58469dc4638
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-20 15:53:29 +0100

    Case Five, 1st number back improved

--- push_swap/case_five.c
+++ push_swap/case_five.c
@@ -6,7 +6,7 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 14:24:18 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/20 15:13:36 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/20 15:52:33 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -15,22 +15,41 @@ void	case_five(t_stack **stack_a, t_stack **stack_b)
 {
 	ft_pab(stack_b, stack_a);
 	ft_pab(stack_b, stack_a);
+	printf("Start Case Three\n");
 	case_three(stack_a, stack_b);
+	printf("End of Case Three\n");
 	ft_pab(stack_a, stack_b);
+
 	if ((*stack_a)->num > (*stack_a)->next->next->next->num)
+	{
+		printf("I.First Letter back Bigger than Last\n");
 		ft_rab(stack_a, stack_b);
-	if ((*stack_a)->num > (*stack_a)->next->num)
-	{
-		printf("1 back bigger then 2");
-		ft_sw(stack_a, stack_b);
 	}
-	if ((*stack_a)->next->num > (*stack_a)->next->next->num)
+	else if ((*stack_a)->num < (*stack_a)->next->next->next->num)
 	{
-		ft_pab(stack_b, stack_a);
-		ft_sw(stack_a, stack_b);
-		ft_pab(stack_a, stack_b);
+		printf("I.First Letter back Smaller than Last\n");
+		if ((*stack_a)->num > (*stack_a)->next->num)
+		{
+			printf("I.First Letter back Bigger than Second\n");
+			ft_sw(stack_a, stack_b);
+			if ((*stack_a)->next->num > (*stack_a)->next->next->num)
+			{
+				printf("I.First letter (now second) Bigger than Third\n");
+				ft_pab(stack_b, stack_a);
+				ft_sw(stack_a, stack_b);
+				ft_pab(stack_a, stack_b);
+			}
+		}
 	}
+	// if ((*stack_a)->next->num > (*stack_a)->next->next->num)
+	// {
+	// 	ft_pab(stack_b, stack_a);
+	// 	ft_sw(stack_a, stack_b);
+	// 	ft_pab(stack_a, stack_b);
+	// }
 	ft_pab(stack_a, stack_b);
+	
+	// OLD
 	if ((*stack_a)->num > (*stack_a)->next->next->next->next->num)
 		ft_rab(stack_a, stack_b);
 	else if ((*stack_a)->num > (*stack_a)->next->num)
