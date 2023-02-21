f0674627892701b6bb60ccbc0674b4a00044e28c
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-21 11:34:19 +0100

    Update Cases Condition Count

--- push_swap/case-controler.c
+++ push_swap/case-controler.c
@@ -6,23 +6,25 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 22:59:44 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/20 18:00:34 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/21 11:33:28 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
 
 void	case_controler(t_stack **stack_a, t_stack **stack_b, int argc)
 {
-	if (argc == 4)
+	if (argc == 3)
 		 case_two(stack_a, stack_b);
-	else if (argc == 5)
+	else if (argc == 4)
 		 case_three(stack_a, stack_b);
-	else if (argc == 6)
+	else if (argc == 5)
 		case_four(stack_a, stack_b);
-	else if (argc == 7)
+	else if (argc == 6)
 		 case_five(stack_a, stack_b);
-	else if (argc == 102)
+	else if (argc >= 101)
 		 case_hun(stack_a);
-	else if (argc == 502)
+	else if (argc >= 501)
 		 case_fhun(stack_a);
+	else 
+		printf("Out Of Range");
 }
