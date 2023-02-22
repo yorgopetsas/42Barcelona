91ef39590f1fa300d43dcae1eb4c790a8cd8bd04
Yorgo Petsas <yorgopecas@gmail.com>
2023-02-22 18:03:28 +0100

    Case Three (+scenarios) Cleaned - Pases Norminette

--- push_swap/case_three.c
+++ push_swap/case_three.c
@@ -6,7 +6,7 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 14:24:13 by yorgopetsas       #+#    #+#             */
-/*   Updated: 2023/02/21 14:29:41 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/02/22 18:02:30 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -36,5 +36,4 @@ void	case_three(t_stack **stack, t_stack **stack_b)
 		else if (b > c && c < a)
 			scenario_5(stack, stack_b);
 	}
-	// show_stack(stack, stack_b);
 }
