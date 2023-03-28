327f18c284c1ffde892476d02b079d6823ff71ad
Yorgo Zisis petsas <yzisis-p@car15s5.42barcelona.com>
2023-03-28 17:45:07 +0200

    Work on new back_2_a algorithm started

--- push_swap/case_hun.c
+++ push_swap/case_hun.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   case_hun.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
+/*   By: yzisis-p <yzisis-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 20:15:40 by yzisis-p          #+#    #+#             */
-/*   Updated: 2023/03/10 12:45:49 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/03/28 17:41:29 by yzisis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "push_swap.h"
 
 void	final_chunk(t_stack **stack_a, t_stack **stack_b, int ul)
@@ -49,24 +50,39 @@ void	sort_ul(t_stack **stack_a, t_stack **stack_b, int ul, int chunk_size)
 		final_chunk(stack_a, stack_b, ul);
 }
 
-void	back_to_a(t_stack **stack_a, t_stack **stack_b, int ul)
+void	back_to_a(t_stack **stack_a, t_stack **stack_b, int ul, int chunk_size)
 {
 	int		ps;
+	int		ps2;
 
 	ps = 0;
-	while ((*stack_b) != NULL)
+	ps2 = 0;
+
+	while (chunk_size > ps2)
 	{
-		if (is_biggiest(stack_b) == 1)
+		if ((*stack_b)->index > ul)
 		{
 			ft_pab(stack_a, stack_b, 1);
-			if ((*stack_b)->next == NULL)
-				ft_pab(stack_a, stack_b, 1);
-			if ((*stack_a)->next != NULL
-				&& (*stack_a)->index > (*stack_a)->next->index)
+			ps++;
+			if ((*stack_a)->index > (*stack_a)->next->index)
+			{
 				ft_rab(stack_a, stack_b, 1);
+			}
 		}
-		ft_rab(stack_b, stack_a, 2);
 	}
+	// while ((*stack_b) != NULL)
+	// {
+	// 	if (is_biggiest(stack_b) == 1)
+	// 	{
+	// 		ft_pab(stack_a, stack_b, 1);
+	// 		if ((*stack_b)->next == NULL)
+	// 			ft_pab(stack_a, stack_b, 1);
+	// 		if ((*stack_a)->next != NULL
+	// 			&& (*stack_a)->index > (*stack_a)->next->index)
+	// 			ft_rab(stack_a, stack_b, 1);
+	// 	}
+	// 	ft_rab(stack_b, stack_a, 2);
+	// }
 }
 
 void	case_hun(t_stack **stack_a, t_stack **stack_b)
@@ -87,6 +103,13 @@ void	case_hun(t_stack **stack_a, t_stack **stack_b)
 		sort_ul(stack_a, stack_b, ul, chunk_size);
 		x++;
 	}
-	ul = 20;
-	back_to_a(stack_a, stack_b, ul);
+	show_stack(stack_a, stack_b);
+	// ul = 100;
+	// x = 0;
+	// while (x < l)
+	// {
+	// 	ul = ul - 20;
+	// 	back_to_a(stack_a, stack_b, ul, chunk_size);
+	// 	x++;
+	// }
 }
