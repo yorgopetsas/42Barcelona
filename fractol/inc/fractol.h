40069281762a700eb1e387119e47c08cab9bb5f9
Yorgo Petsas <yorgopecas@gmail.com>
2023-06-27 12:11:45 +0200

    Print first pixel

--- fractol/inc/fractol.h
+++ fractol/inc/fractol.h
@@ -6,7 +6,7 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:24:58 by yzisis-p          #+#    #+#             */
-/*   Updated: 2023/06/27 10:21:53 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/06/27 11:45:55 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,6 @@
 
 // LIBRARY FOR PRINTF()
 #include <stdio.h>
+
+// MLX LIBRARY 
+#include <mlx/mlx.h>
--- fractol/src/main.c
+++ fractol/src/main.c
@@ -6,17 +6,44 @@
 /*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 10:14:58 by yzisis-p          #+#    #+#             */
-/*   Updated: 2023/06/27 10:44:48 by yorgopetsas      ###   ########.fr       */
+/*   Updated: 2023/06/27 12:10:57 by yorgopetsas      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fractol.h"
+#include "../inc/mlx/mlx.h"
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(void)
 {
-	int	i;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 
-	i = 0;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1080, 780, "Fractol by Dubby");
+	img.img = mlx_new_image(mlx, 1080, 780);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
 
-	printf("%d\n", i);
+	my_mlx_pixel_put(&img, 540, 390, 0x00ff01ff);
+
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+
 }
