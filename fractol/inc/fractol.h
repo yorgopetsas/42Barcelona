dd1c6df17832a6e6c63cd48eee2ea9ca17a9f2ae
Yorgo Petsas <yorgopecas@gmail.com>
2023-06-27 10:52:05 +0200

    Fractol structure first step

---
+++ fractol/inc/fractol.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yorgopetsas <yorgopetsas@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/27 10:24:58 by yzisis-p          #+#    #+#             */
+/*   Updated: 2023/06/27 10:21:53 by yorgopetsas      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// LIBRARY FOR READ()
+#include <unistd.h>
+
+// LIBRARY FOR MALLOC()
+#include <stdlib.h>
+
+// LIBRARY FOR PRINTF()
+#include <stdio.h>
