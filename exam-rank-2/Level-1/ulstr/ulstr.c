0770ec62bcccd8d272fc938db67106dac5a3dd6b
Yorgo Petsas <yorgopecas@gmail.com>
2023-11-07 13:32:45 +0100

    Exam Rank 2 / Level 1 / ulstr

---
+++ exam-rank-2/Level-1/ulstr/ulstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+int	main(int c, char **v)
+{
+	int	i;
+
+	if (c == 2)
+	{
+		i = 0;
+		while (v[1][i])
+		{
+			if (v[1][i] >= 'a' && v[1][i] <= 'z')
+				v[1][i] -= 32;
+			else if (v[1][i] >= 'A' && v[1][i] <= 'Z')
+				v[1][i] += 32;
+			write(1, &v[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
