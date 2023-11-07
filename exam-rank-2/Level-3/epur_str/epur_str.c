4746c684cc1478d692b8451c54fd39929b351b7b
Yorgo Petsas <yorgopecas@gmail.com>
2023-11-07 14:11:24 +0100

    Exam Rank 2 / Level 3 / epur_str

---
+++ exam-rank-2/Level-3/epur_str/epur_str.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	f;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t' || argv[1][i] == '\n')
+			i++;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t' || argv[1][i] == '\n')
+				f = 1;
+			if (!(argv[1][i] == ' ' || argv[1][i] == '\t' || argv[1][i] == '\n'))
+			{
+				if (f)
+					write(1, " ", 1);
+				write(1, &argv[1][i], 1);
+				f = 0;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
