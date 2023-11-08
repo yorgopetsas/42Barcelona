aa926f2d7e530bbcddce596c28d74a6e16e97c58
Yorgo Petsas <yorgopecas@gmail.com>
2023-11-08 04:10:09 +0100

    Exam Rank 2 / Level 4 / sort_int_tab

---
+++ exam-rank-2/Level-4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,18 @@
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	int	tmp;
+	int	i;
+
+	while (i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
